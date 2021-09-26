d3b495e08bf52da736d76c7070abafae7bfc84af
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2021-09-26 13:26:09 -0500

    A version with works with config.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
+#include "../config.h"
+#include <unistd.h>
 #include <iostream>
 #include <cstdlib>
 
-int
-main(int argc, const char* argv[]) {
-  std::cout << "Starting version" << std::endl;
+static void usage(const char* progname) {
+  std::cout << "Usage: " << progname
+	    << " -h" << std::endl;
+  std::cout << "\t" << "Print this message"
+	    << std::endl;
+  std::cout << "       " << progname
+	    << " -v" << std::endl;
+  std::cout << "\t" << "Print the version"
+	    << std::endl;
+  exit(EXIT_SUCCESS);
+}
 
+int
+main(int argc, char **argv) {
+
+  int c; 
+  while ((c = getopt(argc, argv, "hv")) != -1) {
+    switch (c) {
+    case 'v':
+      std::cout << argv[0]
+		<< " version "
+		<< VERSION 
+		<< std::endl;
+      break;
+    case 'h':
+      usage(argv[0]);
+      break;
+    }
+  }
+  
   return EXIT_SUCCESS;
 }
