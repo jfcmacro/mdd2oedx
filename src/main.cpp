38efb9b1ce059a777ca43325c95a499a0f74cac6
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2021-10-10 07:35:06 -0500

    This lexer recognized some tokens, but it has problems with eagerness

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "../config.h"
+#include "mdlexer.h"
+#include "mdtokens.h"
 #include <unistd.h>
 #include <libgen.h>
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <cstdio>
 
 static void usage(const char* progname) {
   std::cout << "Usage: " << progname
@@ -20,7 +23,7 @@ static void usage(const char* progname) {
 static void version(const char* progname) {
   std::cout << progname
 	    << " version "
-	    << VERSION 
+	    << VERSION
 	    << std::endl;
   exit(EXIT_SUCCESS);
 }
@@ -30,8 +33,8 @@ main(int argc, char **argv) {
 
   char *path = strdup(argv[0]);
   char *progname = basename(path);
-  
-  int c; 
+
+  int c;
   while ((c = getopt(argc, argv, "hv")) != -1) {
     switch (c) {
     case 'v':
@@ -42,9 +45,24 @@ main(int argc, char **argv) {
     }
   }
 
-  std::cout << progname
-	    << " not yet implemented"
-	    << std::endl;
-  
+  for (int i = optind; i < argc; ++i) {
+    FILE *fd = nullptr;
+
+    if (strcmp(argv[i], "-") != 0)
+      fd = ::fopen(argv[i], "r");
+    else
+      fd = ::stdin;
+
+    if (fd == nullptr)
+      std::cerr << "Could't open " << argv[i] << std::endl;
+    else {
+      yyset_in(fd);
+      int token;
+      while (token = yylex())
+	std::cout << token << std::endl;
+      fclose(fd);
+    }
+  }
+
   return EXIT_SUCCESS;
 }
---
+++ src/mdtokens.h
@@ -0,0 +1,14 @@
+#pragma once
+
+const int H1 = 1;
+const int H2 = 2;
+const int H3 = 3;
+const int H4 = 4;
+const int H5 = 5;
+const int H6 = 6;
+const int TEXT = 7;
+const int CODE = 8;
+const int ENUM = 9;
+const int BOLD = 10;
+const int EOL = 255;
+
